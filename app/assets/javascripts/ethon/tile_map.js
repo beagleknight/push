c75d50a7a2bb849d7d8e0df57d21d42ebce4679a
david <david@imesmes.com>
2011-09-24 22:01:12 +0200

    Adding first version of tilemap with fake sprites

--- app/assets/javascripts/ethon/tile_map.js
+++ app/assets/javascripts/ethon/tile_map.js
@@ -2,69 +2,68 @@
  * Class TileMap
 */
 
-function TileMap(rows, cols, w, h, step_x, step_y) {
+function TileMap(texture_id, rows, cols, w, h, step_x, step_y) {
+  this.ethon = Ethon.getInstance();
   this.rows = rows;
   this.cols = cols;
   this.tile_w = w;
   this.tile_h = h;
   this.alive = true;
 
-  this.rows_per_canvas = Math.ceil(Game.getInstance().render.canvas_height/this.tile_h);
-  this.cols_per_canvas = Math.ceil(Game.getInstance().render.canvas_width/this.tile_w);
+  this.rows_per_canvas = Math.ceil(this.ethon.render_manager.canvas_height/this.tile_h);
+  this.cols_per_canvas = Math.ceil(this.ethon.render_manager.canvas_width/this.tile_w);
   this.canvas_outside = (this.rows-this.rows_per_canvas)*this.tile_h;
 
   this.map = new Array(rows);
   for(var i = 0; i < rows; i++)
     this.map[i] = new Array(cols); 
-  this.tiles = new Array();
+
+  this.tiles = this.ethon.texture_manager.get_texture(texture_id);
 
   this.offset_x = 0;
   this.step_x = step_x;
   this.offset_y = 0;
   this.step_y = step_y;
-}
 
-TileMap.prototype.add_tile = function(url, w, h) {
-  var sprite = new Sprite(0,0,w,h);
-  sprite.add_frame(url);
-  this.tiles.push(sprite);
-}
+  this.draw = function() {
+    var x = 0; 
+    var y = this.offset_y;
 
-TileMap.prototype.draw = function(render) {
-  var x = 0; 
-  var y = this.offset_y;
+    for(var i = 0; i < this.rows; i++) {
+      for(var j = 0; j < this.cols; j++) {
+        var id = this.map[i][j];
+        var pos = new Vector2D(x,y);
+        var init_x = 0;
+        var init_y = 0;
+        var crop = new Vector2D(id*this.tile_w,0);
+        var dimension = new Vector2D(this.tile_w,this.tile_h);
+        this.ethon.render_manager.renderImage(this.tiles,pos,crop,dimension);
+        x += this.tile_w;
+      }
+      x = 0;
+      y += this.tile_h;
 
-  for(var i = 0; i < this.rows; i++) {
-    for(var j = 0; j < this.cols; j++) {
-      var id = this.map[i][j];
-      var tile = this.tiles[id];
-      tile.pos.x = x;
-      tile.pos.y = y;
-      tile.draw(render);
-      x += tile.w;
+      var canvas_height = this.ethon.render_manager.canvas_height;
+      if(y > canvas_height) {
+        y = y-(canvas_height+this.canvas_outside);
+      }
     }
-    x = 0;
-    y += tile.h;
+  }
 
-    var canvas_height = Game.getInstance().render.canvas_height;
-    if(y > canvas_height) {
-      y = y-(canvas_height+this.canvas_outside);
+  this.update = function(dt) {
+    var canvas_height = this.ethon.render_manager.canvas_height;
+    this.offset_y = this.offset_y+this.step_y; 
+
+    if(this.step < 0) {
+      if(this.offset_y < -this.canvas_outside) {
+        this.offset_y = this.offset_y+canvas_height+this.canvas_outside;
+      }
+    }
+    else {
+      if(this.offset_y > canvas_height) {
+        this.offset_y = this.offset_y-canvas_height-this.canvas_outside;
+      }
     }
   }
 }
 
-TileMap.prototype.update = function() {
-  var canvas_height = Game.getInstance().render.canvas_height;
-  this.offset_y = this.offset_y+this.step_y; 
-
-  if(this.step < 0) {
-    if(this.offset_y < -this.canvas_outside) {
-      this.offset_y = this.offset_y+canvas_height+this.canvas_outside;
-    }
-  }
-  else {
-    if(this.offset_y > canvas_height) {
-      this.offset_y = this.offset_y-canvas_height-this.canvas_outside;
-    }
-  }
-}
--- app/assets/javascripts/push/main.js
+++ app/assets/javascripts/push/main.js
@@ -6,13 +6,22 @@ function Main(ethon) {
   this.ethon = Ethon.getInstance();
 
   this.init = function() {
+    this.ethon.texture_manager.add_texture('background','sprites/background.png');
+    this.background = new TileMap("background",40,20,32,32,0,3);
+    for(var i = 0; i < this.background.rows; i++) {
+      for(var j = 0; j < this.background.cols; j++) {
+        this.background.map[i][j] = rand(0,7); 
+      } 
+    }
   };
   
   this.draw = function() { //render function
+    this.background.draw();
     this.ethon.player.draw();
   };
 
   this.update = function(dt) { //update function
+    this.background.update(dt);
     this.ethon.player.update(dt);
   };
 };
